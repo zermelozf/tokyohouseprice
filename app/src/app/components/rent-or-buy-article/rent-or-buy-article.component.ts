54b166dfa0babdb3606ccd0bc83bb1682b404c70
Arnaud Rachez <arnaud@linalgo.com>
2025-07-12 13:53:41 +0900

    feat: article draft

---
+++ app/src/app/components/rent-or-buy-article/rent-or-buy-article.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-rent-or-buy-article',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './rent-or-buy-article.component.html',
+  styleUrls: ['./rent-or-buy-article.component.scss']
+})
+export class RentOrBuyArticleComponent {
+  constructor() { }
+} 
